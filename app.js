0248b48ab8595d168c47388ff6507cc4bb317580
AngelManuel1995 <angelmanuel.goez@hotmail.es>
2018-03-02 17:15:58 -0500

    yargs added into the project

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 const request = require('request')
+const yargs   = require('yargs')
+
+const argv = yargs.options({
+    a:{
+        demand:true,
+        alias:'address',
+        describe:'Address to fetch weather for',
+        string:true
+    }
+})
+.help()
+.alias('help','h')
+.argv
+
+let encodedAddress = encodeURIComponent(argv.a)
 
 request( { 
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json:true
 },(error, response, body) => {
-    console.log(body)
+    if( error ){
+        console.log("Unable to connect to Google servers.")
+    }else if( body.status === 'ZERO_RESULTS' ){
+        console.log('Unable to find that address')
+    }else if ( body.status === 'OK' ){
+        console.log(`Address: ${body.results[0].formatted_address}`)
+        console.log(`Latitud: ${ body.results[0].geometry.location.lat }`)
+        console.log(`Longitude: ${ body.results[0].geometry.location.lng }`)
+    }
 })
+
+/**
+ * Address to test:
+ * 1301 lombard street philadelphia
+ * 1614 south broad street philadelphia
+ */
